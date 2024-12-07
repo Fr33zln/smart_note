906ca17f94d465122f8f74b2dfca9f1a9164b71d
Fr33zln <fre3ezln@gmail.com>
2024-12-07 13:51:20 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -13,13 +13,13 @@ delete_note = QPushButton("Видалити нотатку")
 create_tag = QPushButton("Додати до тегу")
 delete_tag = QPushButton("Вилучити з тегу")
 search_tag = QPushButton("Шукати тег")
-label_note = QLabel("")
-#delete_tag_note
-label_tag  = QLabel("")
+label_note = QLabel("Створити замітки")
+label_tag  = QLabel("Створити теги")
 note_list_widget = QListWidget()
 tag_list_widget = QListWidget()
-searching_tag_input = QListWidget()
-#text_edit
+searching_tag_input = QLineEdit()
+text_edit = QTextEdit()
+text_edit.setDisabled(True)
 
 layout1 = QHBoxLayout()
 layout1.addWidget(create_note)
@@ -32,15 +32,27 @@ layout2.addWidget(delete_tag)
 
 panel_layout = QVBoxLayout()
 panel_layout.addWidget(label_note)
-panel_layout.addWidget(note_list_widget) 
+panel_layout.addWidget(note_list_widget)
+panel_layout.addLayout(layout1)
 panel_layout.addWidget(delete_note)
 panel_layout.addWidget(label_tag)
 panel_layout.addWidget(tag_list_widget)
+panel_layout.addLayout(layout2)
 panel_layout.addWidget(searching_tag_input)
-#panel_layout.addWidget(text_edit)
+panel_layout.addWidget(text_edit)
 
-#main_layout
-window.setLayout(layout1)
-window.setLayout(layout2)
+main_layout= QHBoxLayout()
+main_layout.addWidget(text_edit)
+main_layout.addLayout(panel_layout)
 
+
+
+window.setLayout(main_layout)
 window.show()
+
+
+
+#скачати з першого сайту get.pip
+#далі там буде боотстрап і зберегти його в програму
+#python get-pip.py написати в терміналі вс коду
+
