ebb6e0f330da97901e69c44dacc4ed1e8970cd17
Fr33zln <fre3ezln@gmail.com>
2024-12-07 13:50:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ def error(txt):
 def show_note():
     note_list_widget.addItems(list(data.keys()))
 
+def show_tag(name_note):
+    tag_list_widget.clear()
+    tag_list_widget.addItems(data[name_note]["TAG"])
+
 
 def add_note():
     name_note = QInputDialog().getText(QInputDialog(), "Назва нотатка", " Введіть назву нотатка")[0]
@@ -34,16 +38,65 @@ def save_file():
     except:
         error("Щоб зберегти, спочатку виберіть нотаток")
 
+
+def remove_note():
+    try:
+        name_note = note_list_widget.currentItem().text()
+        del data[name_note]
+        write_json(name_file, data)
+        note_list_widget.clear()
+        show_note()
+        text_edit.setPlainText("")
+        text_edit.setDisabled(True)
+    except:
+        error("Щоб видалити, спочатку виберіть нотаток")
+
+
+def add_tag():
+    try:
+        name_note = note_list_widget.currentItem().text()
+        name_tag = QInputDialog().getText(QInputDialog(), "Введеня текста", "Введіть назву тега")[0]
+        data[name_note]["TAG"].append(name_tag)
+        write_json(name_note, data)
+        tag_list_widget.addItem(name_tag)
+    
+
+    except:
+        error("Щоб створити тег, спочатку виберіть нотаток")
+
+
+def remove_tag():
+    try:
+        name_note = note_list_widget.currentItem().text()
+        name_tag = tag_list_widget.currentItem().text()
+        data[name_note]["TAG"].remove(name_tag)
+        write_json(name_file,data)
+        show_tag(name_note)
+
+    except:
+        error("Щоб видалити тег, спочатку виберіть нотаток а потім тег")
+
+
+
+
+
+
+
 def show_text():
+    text_edit.setDisabled(False)
     name_note = note_list_widget.currentItem().text()
     text_edit.setPlainText(data[name_note]["TEXT"])
+    show_tag(name_note)
+
+
+
 
 create_note.clicked.connect(add_note)
 create_note.clicked.connect(save_file)
 note_list_widget.clicked.connect(show_text)
-
-
-
+delete_note.clicked.connect(remove_note)
+create_tag.clicked.connect(add_tag)
+delete_tag.clicked.connect(remove_tag)
 
 
 show_note()
